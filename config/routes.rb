1d347462cf49bb39c207cea535c69f9e19a20a4c
naoto7010code <n.m.1210@au.com>
2022-10-25 23:57:20 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  #postsコントローラーのindexアクションを呼び出す
+  root to: 'posts#index'
   post 'posts', to: 'posts#create'
 end
